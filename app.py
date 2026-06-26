a18a99292da0561b444691e18fe1887b5efc2916
nesibra06-droid <nesibra06@gmail.com>
2026-06-26 17:28:59 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import streamlit as st
+from supabase import create_client
+from datetime import datetime
 SUPABASE_URL = st.secrets["SUPABASE_URL"]
 SUPABASE_KEY = st.secrets["SUPABASE_KEY"]supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
